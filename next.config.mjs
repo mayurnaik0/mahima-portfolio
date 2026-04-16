b9e43b5bea3d20ed2f20ad343292994d4859cb8d
mayurnaik0 <mayurnaik5050@gmail.com>
2026-04-16 13:59:48 +0530

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Replace 'mahima-portfolio' with your exact GitHub repository name
+  basePath: '/mahima-portfolio', 
+};
+
+export default nextConfig;
